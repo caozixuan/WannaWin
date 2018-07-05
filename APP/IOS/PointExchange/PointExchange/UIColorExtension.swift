de4cf72b2e757966b18edbc920d1a205a7b812b0
YINNNER <yulianwei1998@163.com>
2018-07-05 15:51:48 +0800

    init ios

---
+++ APP/IOS/PointExchange/PointExchange/UIColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIColorExtension.swift
+//  PointExchange
+//
+//  Created by yiner on 2018/7/5.
+//  Copyright © 2018年 WannaWin. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+	
+	/// VC主背景颜色
+	class func themeBackgroundColor() -> UIColor{
+		return UIColor(red: 39 / 255.0, green: 150 / 255.0, blue: 255 / 255.0, alpha: 1.0);
+	}
+	
+}
