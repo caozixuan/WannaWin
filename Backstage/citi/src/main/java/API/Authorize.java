85963dfb27eca11552355f5eababa89548e51e2f
曹子轩 <caozixuanchina@gmail.com>
2018-07-06 21:22:55 +0800

    增加了花旗接口包，只先写了验证

---
+++ Backstage/citi/src/main/java/API/Authorize.java
@@ -0,0 +1,29 @@
+package API;
+import okhttp3.*;
+
+import java.io.IOException;
+public class Authorize {
+    public static String getAccessToken(){
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "grant_type=client_credentials&scope=/api");
+        Request request = new Request.Builder()
+                .url("https://sandbox.apihub.citi.com/gcb/api/clientCredentials/oauth2/token/au/gcb")
+                .post(body)
+                .addHeader("accept", "application/json")
+                .addHeader("authorization", "Basic NTU0NjUwMjYtMzFkMy00ODgxLWEyOWEtNDE5YzM2NGI2N2RiOlc2dlkwbEg4ckw0c0s2bFg2YlA3YUw2ZUMyd1gxbkczYUc3c1I4dkE4bEEzbVMwZUg2")
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .build();
+
+        try{
+            Response response = client.newCall(request).execute();
+            ResponseBody RB = response.body();
+            System.out.println(RB.string());
+        }
+        catch (IOException e){
+            System.out.println("request error");
+        }
+        return RB.string();
+    }
+}
