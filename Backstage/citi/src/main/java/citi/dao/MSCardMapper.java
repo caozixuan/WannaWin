46350237045eda65d0586a9e41077089938e6fc9
曹子轩 <caozixuanchina@gmail.com>
2018-07-07 17:20:16 +0800

    写mscard包中的代码，需要补充json相关的东西

--- Backstage/citi/src/main/java/citi/dao/MSCardMapper.java
+++ Backstage/citi/src/main/java/citi/dao/MSCardMapper.java
@@ -9,8 +9,12 @@ import java.util.List;
 public interface MSCardMapper {
 
     //插入一个会员卡
+    // 曹子轩：疑问这里int是怎么规定的？
     int insert(MSCard msCard);
 
     //查询用户下的会员卡
     List<MSCard> select(String userID);
+
+    // 曹子轩：疑问：这里是不是缺了一个根据卡号选卡的操作？
+    MSCard selectCard(String cardID);
 }
--- Backstage/citi/src/main/java/citi/dao/MerchantMapper.java
+++ Backstage/citi/src/main/java/citi/dao/MerchantMapper.java
@@ -1,5 +1,6 @@
 package citi.dao;
 
+import citi.vo.MSCardType;
 import citi.vo.Merchant;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,8 @@ public interface MerchantMapper {
     List<Merchant> select(int start, int length);
 
     Merchant select(String merchantID);
+
+    // 曹子轩：返回商户对应的卡类型，这个属于数据库的操作吧？
+    // 返回商家所有有的卡的类型
+    List<MSCardType> selectTypes(String merchantID);
 }
