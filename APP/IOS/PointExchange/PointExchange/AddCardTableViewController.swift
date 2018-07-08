b7af441b9f6c355572218c0ad6a39326c297fe15
YINNNER <yulianwei1998@163.com>
2018-07-08 11:34:48 +0800

    finish AddCardTableViewController 0.2

--- APP/IOS/PointExchange/PointExchange/AddCardTableViewController.swift
+++ APP/IOS/PointExchange/PointExchange/AddCardTableViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class AddCardTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var cardUser: UITextField!
+	
+	@IBOutlet weak var cardId: UITextField!
+	
+	@IBOutlet weak var finishBtn: UITableViewCell!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+		cardUser.delegate = self
+		cardId.delegate = self
+		finishBtn.isUserInteractionEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,65 +28,54 @@ class AddCardTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-//
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 2
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return section
-//    }
-
-//	
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = super.tableView(tableView, cellForRowAt: indexPath)
-//		
-//        return cell
-//    }
-//	
 	// MARK: - TextField delegate
+	func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+		let text1 = cardUser.text
+		let text2 = cardId.text
+		// FIXME: - 后期需要修改条件
+		if text1?.count != 0 &&
+			text2?.count != 0 && isCardIdValid()
+		{
+			finishBtn.isUserInteractionEnabled = true
+			finishBtn.backgroundColor = UIColor.blue
+			
+		}
+		return true
+	}
 	
+	func isCardIdValid() -> Bool {
+		// FIXME: - 后期完善
+		return true
+	}
 	
+	func checkBindVaild() -> Bool {
+		// FIXME: - 后期完善
+		return true
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if indexPath.section == 1 && indexPath.row == 0 {
+			let alert:UIAlertController!
+			if checkBindVaild() {
+				alert = UIAlertController(title:"完成绑定", message:nil, preferredStyle:.alert)
+				let okAction = UIAlertAction(title:"确定", style:.default, handler:{ action in
+					self.navigationController!.popViewController(animated: true)
+					self.navigationController?.popViewController(animated: true)
+				})
+				alert.addAction(okAction)
+			}
+			else {
+				// TODO: - message后期完善
+				alert = UIAlertController(title:"绑定失败", message:nil, preferredStyle:.alert)
+				let cancelAction = UIAlertAction(title:"取消", style:.cancel, handler:nil)
+				alert.addAction(cancelAction)
+			}
 
+			self.present(alert, animated: true, completion: nil)
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+		}
+	}
+	
     /*
     // MARK: - Navigation
 
