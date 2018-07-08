4dc173e868bf897b7580da7dd1a36b802e9cd710
王子昂 <gnaizgnaw@163.com>
2018-07-08 23:28:42 +0800

    完善反馈，添加反馈成功，修改多行输入框

--- APP/Android/PointsLeague/app/src/main/java/com/citiexchangeplatform/pointsleague/FeedbackActivity.java
+++ APP/Android/PointsLeague/app/src/main/java/com/citiexchangeplatform/pointsleague/FeedbackActivity.java
@@ -1,8 +1,12 @@
 package com.citiexchangeplatform.pointsleague;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FeedbackActivity extends AppCompatActivity {
 
@@ -16,6 +20,28 @@ public class FeedbackActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    public void buttonClick(View view){
+        Intent intent = new Intent(FeedbackActivity.this,FeedbackResultActivity.class);
+        //判断输入为空情况
+        TextView title = (TextView) findViewById(R.id.textView_feedback_title);
+        TextView email = (TextView) findViewById(R.id.textView_feedback_email);
+
+        if(title.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "标题不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(email.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "邮箱不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        startActivity(intent);
+
+
+
+
 
 
 
