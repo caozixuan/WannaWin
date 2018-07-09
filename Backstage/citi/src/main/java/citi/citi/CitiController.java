229245c5502722b0adf4ab2c115b05c3cde089fb
曹子轩 <caozixuanchina@gmail.com>
2018-07-09 20:56:13 +0800

    卡的绑定和解绑，还是缺数据库操作

--- Backstage/citi/src/main/java/citi/citi/CitiController.java
+++ Backstage/citi/src/main/java/citi/citi/CitiController.java
@@ -3,12 +3,18 @@ package citi.citi;
 import citi.API.Account;
 import citi.API.Authorize;
 import citi.API.Card;
+import citi.API.Customer;
+import citi.vo.CardDetail;
 import citi.vo.CitiCard;
+import citi.vo.Phone;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @RequestMapping("/citi")
 @Controller
@@ -28,10 +34,33 @@ public class CitiController {
     @ResponseBody
     @RequestMapping("/bindCard")
     public String bindCard(String code){
+        // TODO:怎么获取当前请求的用户？
+        String phoneNum;
+        String creditCardNum;
         String accessInformation = Authorize.getAccessTokenWithGrantType(code,"https://www.baidu.com");
         String accessToken = Authorize.getToken(accessInformation);
+        String refreshAccessToken = Authorize.getRefreshToken(accessInformation);
+        /*
+         TODO:在这里应该将accessToken存入数据库
+         */
         String acountInformation = Account.getAccountInformation(accessToken);
         String cardsInformation = Card.getCardsInformation(accessToken);
+        String phoneInformation = Customer.getCustomerPhone(accessToken);
+        ArrayList<Phone> phones = gson.fromJson(phoneInformation, new TypeToken<ArrayList<Phone>>() {
+        }.getType());
+        if(phones.size()==1){
+            phoneNum = String.valueOf(phones.get(0).getPhoneNum());
+        }
+        ArrayList<CardDetail> cardDetails = gson.fromJson(cardsInformation, new TypeToken<ArrayList<CardDetail>>() {
+        }.getType());
+        if(cardDetails.size()==1){
+            creditCardNum = cardDetails.get(0).getDisplayCardNumber();
+        }
+        /*
+         TODO:拿到手机号和卡号，下面开始数据库的操作，数据库表的设计有问题
+         */
+
+
         /*
           TODO:后面欠json解析和把相关数据存入数据库
           某一个表中是不是要增加refresh_token?
@@ -53,6 +82,15 @@ public class CitiController {
     public String unBind(CitiCard citiCard){
 
 
+        return null;
+    }
+
+    @RequestMapping("/refreshToekn")
+    public String refreshToken(){
+        // 这里同样涉及数据库操作
+        String refreshToken = Authorize.refreshToken();
+        // 把得到的token再次存入数据库
+        // 根据数据库操作结果返回值
         return null;
     }
 
