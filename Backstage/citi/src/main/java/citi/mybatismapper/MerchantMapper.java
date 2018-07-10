36e17a8c11268669832f687bfefa06eb0fa3108c
lzb6666 <31194937+lzb6666@users.noreply.github.com>
2018-07-10 15:04:03 +0800

    mscrad测试上线

--- Backstage/citi/src/main/java/citi/mybatismapper/MerchantMapper.java
+++ Backstage/citi/src/main/java/citi/mybatismapper/MerchantMapper.java
@@ -22,8 +22,7 @@ public interface MerchantMapper {
     //The return sequence will be [start+1, start+2, ,,, start+length].
     @Select(getSome)
     List<Merchant> select(@Param("start") int start,@Param("length") int length);
-    List<Merchant> select(@Param("start") int start, @Param("length") int length);
->>>>>>> b5d2804c0234318c169c9c93d45093a785cdc08b
+
 
     @Select(getById)
     Merchant selectByID(String MerchantID);
