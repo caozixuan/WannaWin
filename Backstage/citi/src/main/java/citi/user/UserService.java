ecc4a26ea3a68ff0a4ec39d47d433eb1916a5f44
lzb6666 <31194937+lzb6666@users.noreply.github.com>
2018-07-11 19:39:08 +0800

    userID获取信息

--- Backstage/citi/src/main/java/citi/user/UserService.java
+++ Backstage/citi/src/main/java/citi/user/UserService.java
@@ -3,10 +3,12 @@ package citi.user;
 import citi.mapper.UserMapper;
 import citi.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by zhong on 2018/7/11 19:23
  */
+@Service
 public class UserService {
     @Autowired
     private UserMapper userMapper;
