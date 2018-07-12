7c864e0f75cf4de46716c0dbc8fbe2f6b77d2ec9
rsy56640 <851911672@qq.com>
2018-07-12 15:39:45 +0800

    update ItemMapper update logo

--- Backstage/citiMerchant/src/main/java/citiMerchant/mapper/ItemMapper.java
+++ Backstage/citiMerchant/src/main/java/citiMerchant/mapper/ItemMapper.java
@@ -15,6 +15,8 @@ public interface ItemMapper {
     final String getItemByMerchantID = "SELECT * FROM item WHERE MerchantID = #{merchantID}";
     final String updateItemByID = "UPDATE item SET name = #{name}, description = #{description}, logoURL = #{logoURL}, originalPrice = #{originalPrice}, points = #{points}, overdueTime = #{overdueTime}, stock = #{stock}" +
             "WHERE ItemID = #{ItemID}";
+    final String updateItemOverdueTimeByID="UPDATE item SET overdueTime = #{overdueTime} WHERE ItemID = #{ItemID}";
+    final String updateItemLogoURLByID = "UPDATE item SET logoURL = #{logoURL} WHERE ItemID = #{ItemID}";
     final String updateItemStockByID = "UPDATE item SET stock = #{stock} WHERE ItemID = #{ItemID}";
     final String deleteItemByID = "DELETE FROM item WHERE ItemID = #{ItemID}";
 
@@ -30,13 +32,20 @@ public interface ItemMapper {
     @Update(updateItemByID)
     int updateItemByID(Item item);
 
+    @Update(updateItemOverdueTimeByID)
+    int updateItemOverdueTimeByID(@Param("overdueTime") String overdueTime, @Param("ItemID") String ItemID);
+
+    @Update(updateItemLogoURLByID)
+    int updateItemLogoURLByID(@Param("logoURL") String logoURL, @Param("ItemID") String ItemID);
+
     @Update(updateItemStockByID)
     int updateItemStockByID(@Param("ItemID") String ItemID, @Param("stock") long stock);
 
     @Delete(deleteItemByID)
     int deleteItemByID(String ItemID);
 
-    final String getItemAmountByMerchantID="SELECT COUNT(*) FROM item WHERE MerchantID = #{merchantID}";
+    final String getItemAmountByMerchantID = "SELECT COUNT(*) FROM item WHERE MerchantID = #{merchantID}";
+
     @Select(getItemByMerchantID)
     int getItemAmountByMerchantID(String merchantID);
 
