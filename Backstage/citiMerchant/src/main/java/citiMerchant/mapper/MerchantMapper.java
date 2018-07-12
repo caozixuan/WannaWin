89fe47d7da1c37d57bdfdaad0855efa5cb9846a7
rsy56640 <851911672@qq.com>
2018-07-12 11:33:31 +0800

    ..

--- Backstage/citiMerchant/src/main/java/citiMerchant/mapper/MerchantMapper.java
+++ Backstage/citiMerchant/src/main/java/citiMerchant/mapper/MerchantMapper.java
@@ -16,7 +16,7 @@ public interface MerchantMapper {
 
     final String addMerchant = "INSERT INTO merchant(MerchantID, name, password, description, address, logoURL, cardLogoURL, proportion) " +
             "VALUES(#{merchantID}, #{name}, #{password}, #{description}, #{address}, #{logoURL}, #{cardLogoURL}, #{proportion})";
-    final String loginMerchant = "SELECT * FROM merchant WHERE MerchantID = #{merchant}";
+    final String loginMerchant = "SELECT * FROM merchant WHERE MerchantID = #{merchantID} AND password = #{password}";
     final String getSome = "SELECT * FROM merchant ORDER BY name LIMIT #{start}, #{length}";
     final String getById = "SELECT * FROM merchant WHERE MerchantID = #{Mercantid}";
 
@@ -24,7 +24,7 @@ public interface MerchantMapper {
     int addMerchant(Merchant merchantDAO);
 
     @Select(loginMerchant)
-    Merchant loginMerchant(String merchantID);
+    Merchant loginMerchant(@Param("merchantID") String merchantID, @Param("password") String password);
 
     //The return sequence will be [start+1, start+2, ,,, start+length].
     @Select(getSome)
