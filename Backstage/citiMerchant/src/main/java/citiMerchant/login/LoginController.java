9243e6208e1a85882f0b8b93d2d032998f9297ac
BenPearson1998 <1042153152@qq.com>
2018-07-12 11:33:08 +0800

    no message

--- Backstage/citiMerchant/src/main/java/citiMerchant/login/LoginController.java
+++ Backstage/citiMerchant/src/main/java/citiMerchant/login/LoginController.java
@@ -1,16 +1,35 @@
 package citiMerchant.login;
 
 import citiMerchant.vo.Merchant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Created by zhong on 2018/7/11 19:51
+ *Created by zhong on 2018/7/11 19:51
  */
-public class LoginController {
+@Controller
 
-    public String login(String username,String password){
+public class LoginController {
+    @Autowired
+    private LoginService loginService;
+
+    @RequestMapping("/login")
+    public ModelAndView login(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("login/login");
+        return mv;
+    }
+
+    @RequestMapping("/loginSubmit")
+    public ModelAndView loginSubmit(String username,String password){
 
         return "";
     }
+
+
 
 
 }
