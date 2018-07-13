6512e2e7ce65f00e32957a75f49321618b76eedd
rsy56640 <851911672@qq.com>
2018-07-13 11:09:08 +0800

    ..

--- Backstage/citiMerchant/src/main/java/citiMerchant/accounting/AccountingService.java
+++ Backstage/citiMerchant/src/main/java/citiMerchant/accounting/AccountingService.java
@@ -1,7 +1,12 @@
 package citiMerchant.accounting;
 
+import citiMerchant.mapper.DBHandler;
+
 /**
  * Created by zhong on 2018/7/11 19:50
  */
 public class AccountingService {
+
+    long totalPoints = DBHandler.Record.coupon_record("00001", 7);
+
 }
