f15a01990e8e0afbe70de0f942970aab090265b4
rsy56640 <851911672@qq.com>
2018-07-13 09:33:07 +0800

    update Merchant+cardDescription, DBHandler

--- Backstage/citi/src/main/resource/sql/coupon_record.sql
+++ Backstage/citi/src/main/resource/sql/coupon_record.sql
@@ -1,9 +1,8 @@
 DELIMITER $$
-CREATE Procedure coupon_record(IN IN_MerchantID VARCHAR(45), IN IN_intervalDate INT, OUT totalPoints BIGINT)
+CREATE Procedure coupon_record(IN IN_MerchantID VARCHAR(45), IN IN_intervalDate INT)
 BEGIN
 SELECT SUM(points)
-FROM (SELECT ItemID, getTime FROM user_coupon) AS user_coupon_tmp NATURAL JOIN (SELECT ItemID, points FROM item WHERE MerchantID = IN_MerchantID) AS item_tmp
-WHERE getTime BETWEEN (NOW() - INTERVAL IN_intervalDate DAY) AND NOW()
-INTO totalPoints;
-SELECT totalPoints;
+FROM coupon_view NATURAL JOIN (SELECT ItemID, points FROM item WHERE MerchantID = IN_MerchantID) AS item_tmp
+WHERE getTime BETWEEN (NOW() - INTERVAL IN_intervalDate DAY) AND NOW();
 END$$
+commit;
--- Backstage/citiMerchant/src/main/java/citiMerchant/mapper/DBHandler.java
+++ Backstage/citiMerchant/src/main/java/citiMerchant/mapper/DBHandler.java
@@ -7,8 +7,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -23,20 +22,33 @@ public class DBHandler {
     public static StrategyMapper strategyMapper;
 
     static final private String resource = "mapper.xml";
-    static private SqlSessionFactoryBuilder sqlSessionFactoryBuilder;
     static private SqlSessionFactory sqlSessionFactory;
+
+    static private String pathname = "./log.txt";
+    static private File log_file;
+    static private BufferedWriter log_writer;
 
     static {
         try {
+
+            //init log_file
+            log_file = new File(pathname);
+
+            //init sqlsession
             Reader reader = Resources.getResourceAsReader(resource);
-            sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-            sqlSessionFactory = sqlSessionFactoryBuilder.build(reader);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+
         } catch (IOException e) {
             //e.printStackTrace();
             System.out.println("\nfail to read mapper.xml\n");
         }
     }
 
+    /****************************        初始化完成        ****************************/
+
+    /*
+     * 返回一个列表：包含该商家的“优惠商品”，“减免策略”，“历史订单”的数量。
+     */
     static public ArrayList<Integer> getAmountByMerchantID(String merchantID) {
         int itemAmount = itemMapper.getItemAmountByMerchantID(merchantID);
         int stategyAmount = strategyMapper.getStrategyAmountByMerchantID(merchantID);
@@ -46,13 +58,25 @@ public class DBHandler {
     }
 
 
+    /*
+     * 用于统计商家的积分流水信息，包含兑入和兑出。
+     */
     static public class Record {
         static public void coupon_record() {
             SqlSession session = sqlSessionFactory.openSession();
-
-
+            long time = System.currentTimeMillis();
+            session.select("coupon_record", );
             session.commit();
             session.close();
+            try {
+                log_writer = new BufferedWriter(new FileWriter(log_file));
+                log_writer.write("\"coupon_record\" elapsed time: " + (System.currentTimeMillis() - time) + "ms\n");
+                log_writer.flush();
+                log_writer.close();
+            } catch (IOException e) {
+                //e.printStackTrace();
+                System.out.println("\nfail to log \"coupon_record\"\n");
+            }
         }
     }
 
