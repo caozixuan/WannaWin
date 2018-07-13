d25e893c770521aff9a78c71dad1eb849832eafb
rsy56640 <851911672@qq.com>
2018-07-13 16:52:17 +0800

    ..

--- Backstage/citiMerchant/src/main/java/citiMerchant/mapper/DBHandler.java
+++ Backstage/citiMerchant/src/main/java/citiMerchant/mapper/DBHandler.java
@@ -14,11 +14,11 @@ import java.util.*;
 @Component
 public class DBHandler {
     @Autowired
-    public static OrderMapper orderMapper;
+    public  OrderMapper orderMapper;
     @Autowired
-    public static ItemMapper itemMapper;
+    public  ItemMapper itemMapper;
     @Autowired
-    public static StrategyMapper strategyMapper;
+    public  StrategyMapper strategyMapper;
 
     static final private String resource = "./src/main/java/citiMerchant/mapper/citiMerchant/mapper.xml";
     static private SqlSessionFactory sqlSessionFactory;
@@ -27,7 +27,6 @@ public class DBHandler {
     static private File log_file;
     static private BufferedWriter log_writer;
 
-    @Autowired
     public DBHandler() {
         try {
             //init log_file
@@ -68,7 +67,7 @@ public class DBHandler {
     /*
      * 返回一个列表：包含该商家的“优惠商品”，“减免策略”，“历史订单”的数量。
      */
-    static public ArrayList<Integer> getAmountByMerchantID(String merchantID) {
+    public ArrayList<Integer> getAmountByMerchantID(String merchantID) {
         long time = System.currentTimeMillis();
         int itemAmount = itemMapper.getItemAmountByMerchantID(merchantID);
         int stategyAmount = strategyMapper.getStrategyAmountByMerchantID(merchantID);
@@ -113,7 +112,7 @@ public class DBHandler {
 
 
         /*
-         * 调用格式：
+         * 调用格式：s
          * 返回7天之内商家"00001"的优惠券积分流入
          * long totalPoints = DBHandler.Record.points_in_record("00001", 7, DBHandler.Record.Record_IN.Coupon);
          */
