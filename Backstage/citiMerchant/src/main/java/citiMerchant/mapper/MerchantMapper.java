5761a9d3d082f574ae5d5c0c4e2ad090957aa9b5
rsy56640 <851911672@qq.com>
2018-07-13 17:36:13 +0800

    add change password

--- Backstage/citiMerchant/src/main/java/citiMerchant/mapper/MerchantMapper.java
+++ Backstage/citiMerchant/src/main/java/citiMerchant/mapper/MerchantMapper.java
@@ -18,6 +18,7 @@ public interface MerchantMapper {
     final String loginMerchant = "SELECT * FROM merchant WHERE MerchantID = #{merchantID} AND password = #{password}";
     final String getSome = "SELECT * FROM merchant ORDER BY name LIMIT #{start}, #{length}";
     final String getById = "SELECT * FROM merchant WHERE MerchantID = #{Mercantid}";
+    final String changePassword = "UPDATE merchant SET password = #{password} WHERE MerchantID = #{merchantID}";
 
     @Insert(addMerchant)
     int addMerchant(Merchant merchantDAO);
@@ -43,5 +44,8 @@ public interface MerchantMapper {
 
     @Select(getById)
     Merchant selectByID(String MerchantID);
+
+    @Update(changePassword)
+    int changePassword(@Param("merchantID") String merchantID, @Param("password") String password);
 
 }
