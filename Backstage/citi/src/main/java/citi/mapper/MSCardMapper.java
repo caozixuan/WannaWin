f86ec202ca4bd9b894620ee9d90da0d9c865dc9a
rsy56640 <851911672@qq.com>
2018-07-14 11:35:08 +0800

    add MSCardMapper.getBy_userID_AND_merchantID

--- Backstage/citi/src/main/java/citi/mapper/MSCardMapper.java
+++ Backstage/citi/src/main/java/citi/mapper/MSCardMapper.java
@@ -1,7 +1,9 @@
 package citi.mapper;
 
 import citi.vo.MSCard;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +13,7 @@ public interface MSCardMapper {
 
     final String insert = "INSERT INTO m_card (cardID, userID, cardNum, points, MerchantID) VALUES (#{cardID}, #{userID}, #{cardNum}, #{points}, #{merchantId})";
     final String getByuserId = "SELECT * FROM m_card WHERE userID = #{userID}";
+    final String getBy_userID_AND_merchantID = "SELECT * FROM m_card WHERE UserID = #{userID} AND MerchantID = #{merchantID}";
     final String getCard = "SELECT * FROM m_card WHERE cardID = #{cardID}";
 
     //插入一个会员卡d
@@ -20,6 +23,9 @@ public interface MSCardMapper {
     //查询用户下的会员卡
     @Select(getByuserId)
     List<MSCard> select(String userID);
+
+    @Select(getBy_userID_AND_merchantID)
+    List<MSCard> getBy_userID_AND_merchantID(@Param("userID") String userID, @Param("merchantID") String merchantID);
 
     @Select(getCard)
     MSCard selectCard(String cardID);
