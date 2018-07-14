d88d36bcee261cee60008a36dcbfc4d03db695d6
rsy56640 <851911672@qq.com>
2018-07-14 17:07:50 +0800

    ..

--- Backstage/citiMerchant/src/main/java/citiMerchant/mapper/OrderMapper.java
+++ Backstage/citiMerchant/src/main/java/citiMerchant/mapper/OrderMapper.java
@@ -47,16 +47,16 @@ public interface OrderMapper {
      *
      */
     @Select(getOrderIDByUserID)
-    List<String> getOrderIDByUserID(@Param("userID") String userID, @Param("intervalTime") String intervalTime);
+    List<String> getOrderIDByUserID(@Param("userID") String userID, @Param("intervalTime") int intervalTime);
 
     @Select(getOrderIDByMerchantID)
-    List<String> getOrderIDByMerchantID(@Param("merchantID") String merchantID, @Param("intervalTime") String intervalTime);
+    List<String> getOrderIDByMerchantID(@Param("merchantID") String merchantID, @Param("intervalTime") int intervalTime);
 
     @Select(getOrderByUserID)
-    List<Order> getOrderByUserID(@Param("userID") String userID, @Param("intervalTime") String intervalTime);
+    List<Order> getOrderByUserID(@Param("userID") String userID, @Param("intervalTime") int intervalTime);
 
     @Select(getOrderByMerchantID)
-    List<Order> getOrderByMerchantID(@Param("merchantID") String merchantID, @Param("intervalTime") String intervalTime);
+    List<Order> getOrderByMerchantID(@Param("merchantID") String merchantID, @Param("intervalTime") int intervalTime);
 
 
     final String getOrderAmount = "SELECT COUNT(*) FROM huaqi.order WHERE MerchantID = #{merchantID}";
