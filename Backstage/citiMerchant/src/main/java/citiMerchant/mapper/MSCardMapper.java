ce9a6d45354deab57a9d2eb63b3d675ee22a821e
rsy56640 <851911672@qq.com>
2018-07-14 16:06:21 +0800

    add 商家发放积分 grantPoints in MSCardMapper

--- Backstage/citiMerchant/src/main/java/citiMerchant/mapper/MSCardMapper.java
+++ Backstage/citiMerchant/src/main/java/citiMerchant/mapper/MSCardMapper.java
@@ -4,6 +4,7 @@ import citiMerchant.vo.MSCard;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public interface MSCardMapper {
     final String insert = "INSERT INTO m_card (userID, cardNum, points, MerchantID) VALUES (#{userID}, #{cardNum}, #{points}, #{merchantId})";
     final String getByuserId = "SELECT * FROM m_card WHERE userID = #{userID}";
     final String getBy_userID_AND_merchantID = "SELECT * FROM m_card WHERE UserID = #{userID} AND MerchantID = #{merchantID}";
+    final String grantPoints = "UPDATE m_card SET Points = Points + #{points} WHERE UserID = #{userID} AND MerchantID = #{merchantID}";
 
     //插入一个会员卡d
     @Insert(insert)
@@ -25,5 +27,8 @@ public interface MSCardMapper {
 
     @Select(getBy_userID_AND_merchantID)
     MSCard getBy_userID_AND_merchantID(@Param("userID") String userID, @Param("merchantID") String merchantID);
+
+    @Update(grantPoints)
+    int grantPoints(@Param("points") int points, @Param("userID") String userID, @Param("merchantID") String merchantID);
 
 }
