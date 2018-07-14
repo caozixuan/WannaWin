1f9bdf52999d68f1eaff8b29cc5f3a6000200964
Pan <810650217@qq.com>
2018-07-14 10:55:30 +0800

    历史订单界面

--- APP/IOS/PointExchange/PointExchange/OrdersTableViewController.swift
+++ APP/IOS/PointExchange/PointExchange/OrdersTableViewController.swift
@@ -14,160 +14,62 @@ import RxDataSources
 class OrdersTableViewController: UIViewController {
     
     @IBOutlet weak var segmentControl: UISegmentedControl!
-    @IBOutlet weak var container: UIView!
+    
+    @IBOutlet weak var willUseContainer: UIView!
+    
+    @IBOutlet weak var expireContainer: UIView!
+    @IBOutlet weak var usedContainer: UIView!
+    
+    // 数据数组
     var willUseOrders:[Order]?
     var usedOrders:[Order]?
     var expireOrders:[Order]?
     
-    // tableView相关
-    var willUseDataSource:RxTableViewSectionedReloadDataSource<SectionModel<String,Order>>?
-    var disposeBag = DisposeBag()
-    /**
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.willUseTableView.rowHeight = 85
-        self.usedTableView.rowHeight = 85
-        self.expireTableView.rowHeight = 85
-    }
-    */
     override func viewWillAppear(_ animated: Bool) {
-        //setDataSource()
-        //bindDataSource()
+        super.viewWillAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-/**
-    /// 设置数据源
-    func setDataSource(){
-        dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String,Order>>(configureCell: {(dataSource, view, indexPath, element) in
-            var cell = UITableViewCell()
-            switch self.segmentControl.selectedSegmentIndex{
-            // 创建未使用页的列表
-            case 0:
-                cell = view.dequeueReusableCell(withIdentifier: "coupon")!
-                // 简介
-                (cell.viewWithTag(1) as! UITextView).text = element.description
-                // 商家名
-                (cell.viewWithTag(2) as! UILabel).text = element.merchantName
-                // 时间
-                (cell.viewWithTag(3) as! UILabel).text = element.date
-            // 创建已使用的列表
-            case 1:
-                // TODO: 根据订单类型来选择显示
-                if element.type == "coupon"{
-                    cell = view.dequeueReusableCell(withIdentifier: "coupon")!
-                    // 简介
-                    (cell.viewWithTag(1) as! UITextView).text = element.description
-                    // 商家名
-                    (cell.viewWithTag(2) as! UILabel).text = element.merchantName
-                    // 时间
-                    (cell.viewWithTag(3) as! UILabel).text = element.date
-                }
-                else{
-                    let cell = view.dequeueReusableCell(withIdentifier: "point")
-                    // 使用的积分数
-                    (cell?.viewWithTag(1) as! UITextView).text = String(format: "%f 通用积分", arguments: [element.points!])
-                    // 商家名
-                    (cell?.viewWithTag(2) as! UILabel).text = element.merchantName
-                    // 时间
-                    (cell?.viewWithTag(3) as! UILabel).text = element.date
-                }
-            // 创建过期页的列表
-            case 2:
-                // TODO: 根据订单类型来选择显示
-                if element.type == "coupon"{
-                    cell = view.dequeueReusableCell(withIdentifier: "coupon")!
-                    // 简介
-                    (cell.viewWithTag(1) as! UITextView).text = element.description
-                    // 商家名
-                    (cell.viewWithTag(2) as! UILabel).text = element.merchantName
-                    // 时间
-                    (cell.viewWithTag(3) as! UILabel).text = element.date
-                }
-                else{
-                    cell = view.dequeueReusableCell(withIdentifier: "point")!
-                    // 使用的积分数
-                    (cell.viewWithTag(1) as! UITextView).text = String(format: "%f 通用积分", arguments: [element.points!])
-                    // 商家名
-                    (cell.viewWithTag(2) as! UILabel).text = element.merchantName
-                    // 时间
-                    (cell.viewWithTag(3) as! UILabel).text = element.date
-                }
-            default:
-                break;
-            }
-            return cell
-        })
-        
-    }
     
-    /// 绑定数据源
-    func bindDataSource(){
-        
-        switch self.segmentControl.selectedSegmentIndex {
+    @IBAction func segmentChange(_ sender: Any) {
+        switch segmentControl.selectedSegmentIndex {
         case 0:
-            if let orders = willUseOrders{
-                let obs = Observable.just([
-                    SectionModel(model:"",items:orders)
-                    ])
-                obs.bind(to: self.tableView.rx.items(dataSource: dataSource!))
-                    .disposed(by:disposeBag)
-            }
+            willUseContainer.isHidden = false
+            expireContainer.isHidden = true
+            usedContainer.isHidden = true
         case 1:
-            if let orders = usedOrders{
-                var coupons=[Order]()
-                var points = [Order]()
-                for o in orders{
-                    if o.type == "coupon"{
-                        coupons.append(o)
-                    }
-                    else{
-                        points.append(o)
-                    }
-                }
-                let obs = Observable.just([
-                    SectionModel(model:"使用优惠券",items:coupons),
-                    SectionModel(model:"使用积分",items:points)
-                    ])
-                obs.bind(to: self.tableView.rx.items(dataSource: dataSource!))
-                    .disposed(by:disposeBag)
-                dataSource?.titleForHeaderInSection = { ds, index in
-                    return ds.sectionModels[index].model
-                }
-            }
+            willUseContainer.isHidden = true
+            expireContainer.isHidden = true
+            usedContainer.isHidden = false
         case 2:
-            if let orders = usedOrders{
-                var coupons=[Order]()
-                var points = [Order]()
-                for o in orders{
-                    if o.type == "coupon"{
-                        coupons.append(o)
-                    }
-                    else{
-                        points.append(o)
-                    }
-                }
-                let obs = Observable.just([
-                    SectionModel(model:"使用优惠券",items:coupons),
-                    SectionModel(model:"使用积分",items:points)
-                    ])
-                obs.bind(to: self.tableView.rx.items(dataSource: dataSource!))
-                    .disposed(by:disposeBag)
-                dataSource?.titleForHeaderInSection = { ds, index in
-                    return ds.sectionModels[index].model
-                }
-            }
+            willUseContainer.isHidden = true
+            expireContainer.isHidden = false
+            usedContainer.isHidden = true
         default:
-            break;
+            break
         }
     }
     
-    @IBAction func segmentControlChange(_ sender: Any) {
-        bindDataSource()
-        self.tableView.reloadData()
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "willUseView":
+            if let view = segue.destination as? SingleSectionOrderViewController{
+                view.orders = self.willUseOrders
+            }
+        case "usedView":
+            if let view = segue.destination as? DoubleSectionOrderViewController{
+                view.orders = self.usedOrders
+            }
+        case "expireView":
+            if let view = segue.destination as? SingleSectionOrderViewController{
+                view.orders = self.expireOrders
+            }
+        default:
+            break
+        }
     }
-*/
+
  }
--- APP/IOS/PointExchange/PointExchange/SingleSectionOrderViewController.swift
+++ APP/IOS/PointExchange/PointExchange/SingleSectionOrderViewController.swift
@@ -7,13 +7,46 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+import RxDataSources
 
 class SingleSectionOrderViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    var orders:[Order]?
+    
+    // tableView设置相关
+    var dataSource:RxTableViewSectionedReloadDataSource<SectionModel<String,Order>>?
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.rowHeight = 85
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String,Order>>(configureCell: {(dataSource, view, indexPath, element) in
+            var cell = UITableViewCell()
+            // 创建列表
+            cell = view.dequeueReusableCell(withIdentifier: "coupon")!
+            // 简介
+            (cell.viewWithTag(1) as! UITextView).text = element.description
+            // 商家名
+            (cell.viewWithTag(2) as! UILabel).text = element.merchantName
+            // 时间
+            (cell.viewWithTag(3) as! UILabel).text = element.date
+            return cell
+        })
+        if let order = orders{
+            let obs = Observable.just([
+                SectionModel(model:"",items:order)
+                ])
+            obs.bind(to: self.tableView.rx.items(dataSource: dataSource!))
+                .disposed(by:disposeBag)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +54,5 @@ class SingleSectionOrderViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
