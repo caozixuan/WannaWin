3de113299140e586033d665b426376533efcfc6b
rsy56640 <851911672@qq.com>
2018-07-16 17:03:42 +0800

    test record

--- Backstage/citiMerchant/src/main/java/citiMerchant/test/TestController.java
+++ Backstage/citiMerchant/src/main/java/citiMerchant/test/TestController.java
@@ -1,9 +1,12 @@
 package citiMerchant.test;
 
 import citiMerchant.vo.Record;
+import citiMerchant.vo.RecordOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.sql.Date;
 
 
 @Controller
@@ -16,10 +19,11 @@ public class TestController {
     public void getRecord() {
         System.out.println("aaaaa");
         Record record1 = testService.getCouponRecord("00001", 7);
-        System.out.println("totalPoints of coupons in 7 days: " + record1.getTotalPoints());
+        System.out.println("totalPoints of coupons in 7 days of merchantID\"00001\": " + record1.getTotalPoints());
         Record record2 = testService.getOrderRecord("00002", 7);
-        System.out.println("totalPoints of orders in 7 days: " + record2.getTotalPoints());
-
+        System.out.println("totalPoints of orders in 7 days of merchantID\"00002\": " + record2.getTotalPoints());
+        RecordOrder recordOrder = testService.getOrderRecord_ByDate("00002", new Date(1531065600 * 1000), new Date(1531324800 * 1000));
+        System.out.println("totalPoints of orders from 2018-07-09 to 2018-07-12 of merchantID\"00002\": " + recordOrder.getTotalPoints());
     }
 
 
