4123ec97f2ff3e84743508c886257ef8f550c1bb
rsy56640 <851911672@qq.com>
2018-07-17 09:53:57 +0800

    add Calendar operation

--- Backstage/citiMerchant/src/main/java/citiMerchant/test/TestController.java
+++ Backstage/citiMerchant/src/main/java/citiMerchant/test/TestController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 @Controller
@@ -25,13 +27,28 @@ public class TestController {
         Record record2 = testService.getOrderRecord("00002", 7);
         System.out.println("totalPoints of orders in 7 days of merchantID\"00002\": " + record2.getTotalPoints());
 
-        Date start = new Date(testService.Date2UNIXTime("2018-07-09"));
-        Date end = new Date(testService.Date2UNIXTime("2018-07-12"));
+        final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date date = new java.util.Date();
+        try {
+            date = format.parse("2018-07-09");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        String start_day = format.format(calendar.getTime());
+        calendar.add(Calendar.DAY_OF_YEAR, 3);
+        String end_day = format.format(calendar.getTime());
+
+        java.sql.Date start = new java.sql.Date(testService.Date2UNIXTime(start_day));
+        java.sql.Date end = new java.sql.Date(testService.Date2UNIXTime(end_day));
         RecordOrder recordOrder = testService.getOrderRecord_ByDate("00002", start, end);
         System.out.println("totalPoints of orders from 2018-07-09 to 2018-07-12 of merchantID\"00002\": " + recordOrder.getTotalPoints());
 
 
     }
+
+
 
 
 }
