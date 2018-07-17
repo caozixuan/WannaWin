0efd1adb4e11a17662d494d0dd890f1ba2f09f48
王子昂 <gnaizgnaw@163.com>
2018-07-17 08:45:02 +0800

    积分兑换记录

---
+++ APP/Android/PointsLeague/app/src/main/java/com/citiexchangeplatform/pointsleague/BaseViewHolder.java
@@ -0,0 +1,10 @@
+package com.citiexchangeplatform.pointsleague;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class BaseViewHolder extends RecyclerView.ViewHolder {
+    public BaseViewHolder(View itemView) {
+        super(itemView);
+    }
+}
