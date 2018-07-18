df81585a873688d0ed9a15854b4ef7138d660e36
lzb6666 <31194937+lzb6666@users.noreply.github.com>
2018-07-18 14:55:11 +0800

    发现页功能实现

--- Backstage/citi/src/main/java/citi/item/ItemService.java
+++ Backstage/citi/src/main/java/citi/item/ItemService.java
@@ -2,6 +2,7 @@ package citi.item;
 
 import citi.mapper.CouponMapper;
 import citi.mapper.ItemMapper;
+import citi.support.status.Status;
 import citi.vo.Item;
 import citi.vo.UserCoupon;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,25 +23,28 @@ public class ItemService {
     private CouponMapper couponMapper;
 
     public List<Item> getItems(int start,int length){
-        List<Item> items=new ArrayList<>();
-
-        return items;
+        return itemMapper.getItem(start,length);
     }
 
-    public boolean bug(String userID,String itemID){
+    public Status<Boolean,String> buy(String userID, String itemID,int count){
         Item item=itemMapper.getItemByItemID(itemID);
         if (item==null){
-            return false;
+            return ItemStatus.INVALID;
         }
-        if (item.getStock()<=0){
-            return false;
+        if (item.getStock()-count<0){
+            return ItemStatus.EMPTY;
         }
-        if (itemMapper.updateItemStockByID(itemID,item.getStock()-1)==1){
+        if (itemMapper.updateItemStockByID(itemID,item.getStock()-count)==1){
             UserCoupon userCoupon=new UserCoupon(userID,itemID,UserCoupon.CouponState.UNUSED);
-            if (couponMapper.addUserCoupon(userCoupon)==1){
-                return true;
+            for (int i=0;i<count;i++){
+                couponMapper.addUserCoupon(userCoupon);
             }
+            return ItemStatus.SUCCESS;
         }
-        return false;
+        return ItemStatus.OTHER;
+    }
+
+    public List<Item> getMerchantItems(String merchantID,int start,int length){
+        return itemMapper.getItemByMerchantID(merchantID,start,length);
     }
 }
