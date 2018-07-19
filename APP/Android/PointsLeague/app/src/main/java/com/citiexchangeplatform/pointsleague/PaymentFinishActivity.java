4c8930dd6a964787b36ca85e5d8e6fc595fb96ef
王子昂 <gnaizgnaw@163.com>
2018-07-19 11:02:26 +0800

    当状态栏背景为白色时，设置状态栏颜色为暗色

--- APP/Android/PointsLeague/app/src/main/java/com/citiexchangeplatform/pointsleague/PaymentFinishActivity.java
+++ APP/Android/PointsLeague/app/src/main/java/com/citiexchangeplatform/pointsleague/PaymentFinishActivity.java
@@ -146,6 +146,10 @@ public class PaymentFinishActivity extends AppCompatActivity {
             isImmersive = true;
         }
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {//android6.0以后可以对状态栏文字颜色和图标进行修改
+            getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
         final TitleBar titleBar = (TitleBar) findViewById(R.id.title_bar);
         titleBar.setDividerColor(Color.GRAY);
         //左侧
