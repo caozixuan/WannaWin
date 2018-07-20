566e62ad5dc89e93ebcdbf1c68b178efeb38e51a
Pan <810650217@qq.com>
2018-07-20 22:11:37 +0800

    更新兑换页面UI

--- APP/IOS/PointExchange/PointExchange/ShadowView.swift
+++ APP/IOS/PointExchange/PointExchange/ShadowView.swift
@@ -9,14 +9,11 @@
 import UIKit
 
 class ShadowView: UIView {
-	
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-		//self.setUp()
-	}
+    
+    
 	
 	required init?(coder aDecoder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
+		super.init(coder: aDecoder)
 	}
 	
 	override func awakeFromNib() {
@@ -28,8 +25,18 @@ class ShadowView: UIView {
 		self.layer.cornerRadius = 14
 		self.layer.shadowColor = UIColor(red: CGFloat(132/255.0), green: 143/255.0, blue: 161/255.0, alpha: 0.5/1.0).cgColor
 		self.layer.shadowOffset = CGSize(width:0, height:2)
-		self.layer.shadowOpacity = 0.5;
+		self.layer.shadowOpacity = 1;
 		self.layer.shadowRadius = 5;
+        
+        let middleView = UIView(frame:CGRect(x: self.bounds.width/4, y: 0, width: self.bounds.width/2, height: self.bounds.height))
+        middleView.backgroundColor = UIColor(red: CGFloat(250/255.0), green: 250/255.0, blue: 250/255.0, alpha: 1.0)
+        middleView.layer.shadowColor = UIColor(red: CGFloat(119/255.0), green: 110/255.0, blue: 110/255.0, alpha: 0.5/1.0).cgColor
+        middleView.layer.shadowOffset = CGSize(width:2, height:0)
+        middleView.layer.shadowOpacity = 0.5;
+        middleView.layer.shadowRadius = 4;
+        
+        self.addSubview(middleView)
+        
 	}
 	
 }
