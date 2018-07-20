142a7ae852020f04ae8f625ff847638fe0f2f158
王子昂 <gnaizgnaw@163.com>
2018-07-20 14:58:45 +0800

    细节修改

--- APP/Android/PointsLeague/app/src/main/java/com/citiexchangeplatform/pointsleague/PointsExchangeExpandListActivity.java
+++ APP/Android/PointsLeague/app/src/main/java/com/citiexchangeplatform/pointsleague/PointsExchangeExpandListActivity.java
@@ -1,5 +1,7 @@
 package com.citiexchangeplatform.pointsleague;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +9,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 
@@ -46,6 +50,7 @@ public class PointsExchangeExpandListActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_expandlist);
         final RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        toolBar();
 
         vertical(recyclerView);
 
@@ -101,6 +106,66 @@ public class PointsExchangeExpandListActivity extends AppCompatActivity {
 
     }
 
+    public void toolBar(){
+        boolean isImmersive = false;
+        if (hasKitKat() && !hasLollipop()) {
+            isImmersive = true;
+            //透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        } else if (hasLollipop()) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    //| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            isImmersive = true;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {//android6.0以后可以对状态栏文字颜色和图标进行修改
+            getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        final TitleBar titleBar = (TitleBar) findViewById(R.id.title_bar);
+        titleBar.setDividerColor(Color.GRAY);
+        titleBar.setLeftImageResource(R.drawable.ic_left_black_24dp);
+        titleBar.setLeftText("返回");
+        titleBar.setLeftTextColor(Color.BLACK);
+        titleBar.setLeftClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        titleBar.setTitle("积分兑换记录");
+        titleBar.setTitleColor(Color.BLACK);
+
+        //titleBar.setActionTextColor(Color.BLACK);
+        //
+        //
+        //titleBar.addAction(new TitleBar.TextAction("发布") {
+        //    @Override
+        //    public void performAction(View view) {
+        //        Toast.makeText(PayCodeActivity.this, "点击了发布", Toast.LENGTH_SHORT).show();
+        //    }
+        //});
+
+        //沉浸式
+        titleBar.setImmersive(isImmersive);
+    }
+
+    public static boolean hasKitKat() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+    }
+
+    public static boolean hasLollipop() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
 
     private void getPointsExchangeRecordRequest() {
         recordList = new ArrayList<>();
