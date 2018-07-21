55ff1c73471d87eeb97622ee7abda0b89aa83b2e
YINNNER <yulianwei1998@163.com>
2018-07-21 21:58:55 +0800

    clear useless code

--- APP/IOS/PointExchange/PointExchange/MaskView.swift
+++ APP/IOS/PointExchange/PointExchange/MaskView.swift
@@ -11,9 +11,10 @@ import UIKit
 class MaskView: UIView {
 
 	override func draw(_ rect: CGRect) {
-		// Drawing code
+		// 画半弧形蒙版
 		let color = UIColor.white
 		color.set() // 设置线条颜色
+		
 		let width = self.bounds.size.width
 		let height = self.bounds.size.height
 		let h = 1*height/15
@@ -23,21 +24,13 @@ class MaskView: UIView {
 		
 		let path = UIBezierPath(arcCenter: center, radius: radius , startAngle:(CGFloat)(19*Double.pi/12), endAngle: (CGFloat)(17*Double.pi/12), clockwise:false)
 		
-		//aPath.move(to: CGPoint(x: 0, y: h))
 		path.addLine(to: CGPoint(x:0, y:height))
-		
-		//aPath.move(to: CGPoint(x: 0, y: height))
 		path.addLine(to: CGPoint(x:width, y:height))
-		
-		//aPath.move(to: CGPoint(x: width, y: height))
 		path.addLine(to: CGPoint(x:width, y:h))
 		
 		path.close()
-		path.lineWidth = 5.0 // 线条宽度
-		
-		path.stroke() // Draws line 根据坐标点连线，不填充
-		path.fill() // Draws line 根据坐标点连线，填充
-		
+		path.lineWidth = 5.0
+		path.fill()
 	}
 
 }
