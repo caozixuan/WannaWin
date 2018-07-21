022934d20854eff46506208d351f7187aa73734b
曹子轩 <caozixuanchina@gmail.com>
2018-07-21 10:42:59 +0800

    兑换积分不为负数

--- Backstage/citi/src/main/java/citi/funcModule/points/PointsService.java
+++ Backstage/citi/src/main/java/citi/funcModule/points/PointsService.java
@@ -43,6 +43,11 @@ public class PointsService {
                 returnMerchants.add(returnMerchant);
                 isCanChange = false;
             }
+            else if(Double.valueOf(merchantBean.getSelectedMSCardPoints())<0){
+                ReturnMerchant returnMerchant = new ReturnMerchant(merchantBean.getMerchantID(),merchant.getName(),merchant.getCardLogoURL(), "兑换积分不为负");
+                returnMerchants.add(returnMerchant);
+                isCanChange = false;
+            }
             else if(msCard.getPoints()*merchant.getProportion()<Double.valueOf(merchantBean.getSelectedMSCardPoints())*merchant.getProportion()){
                 ReturnMerchant returnMerchant = new ReturnMerchant(merchantBean.getMerchantID(),merchant.getName(),merchant.getCardLogoURL(), "卡内积分不足");
                 returnMerchants.add(returnMerchant);
