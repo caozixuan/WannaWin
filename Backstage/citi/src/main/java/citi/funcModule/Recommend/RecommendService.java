00b8d8345a7fb4be478de6fd506edd500085b75b
BenPearson1998 <1042153152@qq.com>
2018-08-12 00:43:41 +0800

    商品推荐基本盘上传

--- Backstage/citi/src/main/java/citi/funcModule/Recommend/RecommendService.java
+++ Backstage/citi/src/main/java/citi/funcModule/Recommend/RecommendService.java
@@ -83,11 +83,162 @@ public class RecommendService {
     /**
      * 返回用户的推荐商品列表
      * @param userID
+     * @param num
      * @return ArrayList<Item>
      */
-    public ArrayList<Item> getRecommendedItems(String userID){
-
+    public ArrayList<Item> getRecommendedItems(String userID,int num){
+        ArrayList<UserItemPoints> userItemPointsArrayList = getUserInterestToItems(userID);
+        ArrayList<Item> items = new ArrayList<Item>();
+        for(int i =0;i<num;i++){
+            items.add(itemMapper.getItemByItemID(userItemPointsArrayList.get(i).itemID));
+        }
+        return items;
     }
+    /**
+     * 返回用户对商品喜好评分
+     * @param userID
+     * @return ArrayList<Merchant>
+     */
+    public double getItemPoints(String userID, String itemID){
+        double points = 0;
+        // 目前制定的积分策略：购买一次得5分+浏览一次得1分+每符合一个喜好得5分
+        // TODO:这里需要数据库写两个vo类还有对应mapper里的方法
+        List<Order> orderList = orderMapper.getOrderByUserAndItemID(userID,"+010101010101");
+        List<UserPref> prefList = prefMapper.getPrefByUserID(userID);
+        List<Record> recordList = recordMapper.getRecordByUserID(userID);
+        Item item = itemMapper.getItemByItemID(itemID);
+        points += 5*orderList.size();
+        points += recordList.size();
+        for(int i=0;i<prefList.size();i++){
+            if(prefList.get(i).getPref().equals(item.getItemType())){
+                points+=5;
+            }
+        }
+        return points;
+    }
+
+    class ItemPoints{
+        String itemID;
+        double[] points;
+
+        public ItemPoints(String itemID, double[] points) {
+            this.itemID = itemID;
+            this.points = points;
+        }
+    }
+    class UserItemPoints implements Comparable<UserItemPoints>{
+        String itemID;
+        double points;
+
+        public UserItemPoints(String itemID, double points) {
+            this.itemID = itemID;
+            this.points = points;
+        }
+
+        @Override
+        public int compareTo(UserItemPoints o) {
+            if((this.points - o.points)>=0){
+                return 1;
+            }
+            else return -1;
+        }
+    }
+
+    class ItemSimilarity{
+        String itemID1;
+        String itemID2;
+        double similarity;
+
+        public ItemSimilarity(String itemID1, String itemID2, double similarity) {
+            this.itemID1 = itemID1;
+            this.itemID2 = itemID2;
+            this.similarity = similarity;
+        }
+    }
+    /*
+    * 获取用户商品评分数组
+    */
+    public ArrayList<ItemPoints> getItemPointsArray(){
+        ArrayList<ItemPoints> results = new ArrayList<ItemPoints>();
+        // TODO:这里缺获取所用用户userID的方法
+        ArrayList<String> userIDs = new ArrayList<String>();
+        ArrayList<String> itemIDs = new ArrayList<String>();
+        for(String itemID:itemIDs){
+            ArrayList<Double> points = new ArrayList<Double>();
+            for(String userID: userIDs){
+                points.add(getMerchantPoints(userID,itemID));
+            }
+            double[] pointsArray = new double[points.size()];
+            for(int i=0;i<points.size();i++){
+                pointsArray[i]=points.get(i);
+            }
+            ItemPoints itemPoints = new ItemPoints(itemID,pointsArray);
+            results.add(itemPoints);
+        }
+        try {
+            ObjectOutputStream os = new ObjectOutputStream(
+                    new FileOutputStream("ItemSimilarity.txt"));
+            os.writeObject(results);// 将List列表写进文件
+            os.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
+    public ArrayList<ItemSimilarity> getItemSimilarities(){
+        ArrayList<ItemSimilarity> results = new ArrayList<ItemSimilarity>();
+        ArrayList<ItemPoints> itemPoints = getItemPointsArray();
+        for(int i=0;i<itemPoints.size()-1;i++){
+            for(int j=i;j<itemPoints.size();j++){
+                double similarity = cosineSimilarity(itemPoints.get(i).points,itemPoints.get(j).points);
+                String itemID1 = itemPoints.get(i).itemID;
+                String itemID2 = itemPoints.get(j).itemID;
+                results.add(new ItemSimilarity(itemID1,itemID2, similarity));
+            }
+        }
+        return results;
+    }
+    /*
+   * 返回用户对商品的评分
+   */
+    public ArrayList<UserItemPoints> getUserPointsToItems(String userID){
+        ArrayList<UserItemPoints> results = new ArrayList<UserItemPoints>();
+        ArrayList<String> itemIDs = new ArrayList<>();
+        //TODO:itemMapper中返回所有ItemID
+        itemIDs.addAll(itemMapper,getItemIDs());
+        ArrayList<Item> items = new ArrayList<Item>();
+        for(String id:itemIDs){
+            items.add(itemMapper.getItemByItemID(id));
+        }
+        for(Item item:items){
+            double points = getItemPoints(userID,item.getItemID());
+            results.add(new UserItemPoints(item.getItemID(),points));
+        }
+        return results;
+    }
+ /*
+     * 获得最终用户对商品的喜好评分，并从大到小排序
+     */
+
+    public ArrayList<UserItemPoints> getUserInterestToItems(String userID){
+        ArrayList<ItemSimilarity> similarities = getItemSimilarities();
+        ArrayList<UserItemPoints> userItemPointsArrayList = getUserPointsToItems(userID);
+        ArrayList<UserItemPoints> results = new ArrayList<UserItemPoints>();
+        for(UserItemPoints userItemPoints:userItemPointsArrayList){
+            double points = 0;
+            for(ItemSimilarity similarity:similarities){
+                if(similarity.itemID1.equals(userItemPoints.itemID)||similarity.itemID2.equals(userItemPoints.itemID)){
+                    points+=userItemPoints.points*similarity.similarity;
+                }
+            }
+            results.add(new UserItemPoints(userItemPoints.itemID,points));
+        }
+        Collections.sort(results);
+        return results;
+    }
+
 
     /**
      * 返回用户的推荐商户列表
