d1e55ce35139205fa3736691ee65103753635330
Ruikang Liu <15666613559@163.com>
2018-08-13 16:40:48 +0800

    todo: card detail info

--- APP/Android/PointsLeague/app/src/main/java/com/citiexchangeplatform/pointsleague/adapter/HistoryExchangeAdapter.java
+++ APP/Android/PointsLeague/app/src/main/java/com/citiexchangeplatform/pointsleague/adapter/HistoryExchangeAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.citiexchangeplatform.pointsleague.R;
@@ -18,12 +19,16 @@ public class HistoryExchangeAdapter extends RecyclerView.Adapter<HistoryExchange
         public final TextView textViewPoints;
         public final TextView textViewExchangePoints;
         public final TextView textViewDate;
+        public final ImageView imageView;
+        public final View timeLine;
 
         public VH(View v) {
             super(v);
             textViewPoints = (TextView) v.findViewById(R.id.textView_points_history_exchange_item);
             textViewExchangePoints = (TextView) v.findViewById(R.id.textView_exchange_points_history_exchange_item);
             textViewDate = (TextView) v.findViewById(R.id.textView_date_history_exchange_item);
+            imageView = (ImageView) v.findViewById(R.id.imageView_history_exchange_item);
+            timeLine = (View) v.findViewById(R.id.timeline);
         }
     }
 
@@ -37,9 +42,24 @@ public class HistoryExchangeAdapter extends RecyclerView.Adapter<HistoryExchange
 
     @Override
     public void onBindViewHolder(HistoryExchangeAdapter.VH holder, final int position) {
-        holder.textViewPoints.setText(String.valueOf(items.get(position).getPoint()));
-        holder.textViewExchangePoints.setText(String.format("%.1f",items.get(position).getExchangePoint()));
+        holder.textViewPoints.setText("-" + items.get(position).getPoint());
+        holder.textViewExchangePoints.setText(String.format("+%.1fP",items.get(position).getExchangePoint()));
         holder.textViewDate.setText(items.get(position).getDate());
+        if(position == 0){
+            holder.timeLine.setVisibility(View.INVISIBLE);
+        }
+        switch (position%3)
+        {
+            case 0:
+                holder.imageView.setImageResource(R.drawable.ic_dot_red_24dp);
+                break;
+            case 1:
+                holder.imageView.setImageResource(R.drawable.ic_dot_blue_24dp);
+                break;
+            case 2:
+                holder.imageView.setImageResource(R.drawable.ic_dot_green_24dp);
+                break;
+        }
     }
 
     @Override
