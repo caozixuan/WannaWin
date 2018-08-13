1f98202697cc28610b7728ccdeb9b67e85985b5e
PAN <810650217@qq.com>
2018-08-14 01:07:10 +0800

    会员卡详情

--- APP/IOS/PointExchange/PointExchange/CardDetailViewController.swift
+++ APP/IOS/PointExchange/PointExchange/CardDetailViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class CardDetailViewController: UIViewController,UITableViewDataSource {
+class CardDetailViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+	
+	
+	@IBOutlet weak var barView: GradientView!
+	@IBOutlet weak var barImage: UIImageView!
+	@IBOutlet weak var backgroundView: UIView!
+	@IBOutlet weak var cardInfoBackgroundImage: UIImageView!
+	@IBOutlet weak var barButton: UIButton!
 	
 	var merchantID:String?
 	var indicator:UIActivityIndicatorView?
 	var card:Card?
+	var isFold = true
 	
 	@IBOutlet weak var cardImageView: UIImageView!
 	
@@ -21,7 +29,10 @@ class CardDetailViewController: UIViewController,UITableViewDataSource {
 	override func viewDidLoad() {
         super.viewDidLoad()
 		self.tableView.dataSource = self
-		self.tableView.layer.zPosition = 10
+		self.tableView.delegate = self
+		self.tableView.rowHeight = 56
+		// TODO: 条形码内容
+		self.barImage.image = ScanCodeManager().createBarCode(url:"hi")
 		// 加入“历史积分兑换记录”按钮在导航栏右边
 		let historyBtn = UIBarButtonItem(title: "兑换记录", style: .plain, target: self, action: #selector(goExchangeHistoryVC))
 		self.navigationItem.rightBarButtonItem = historyBtn
@@ -36,6 +47,9 @@ class CardDetailViewController: UIViewController,UITableViewDataSource {
 			}
 			self.indicator?.stopAnimating()
 		}
+		self.tableView.layer.zPosition = 10.0
+		self.barButton.layer.zPosition = 10.0
+		self.cardInfoBackgroundImage.layer.zPosition = 10.0
     }
 	
     // MARK: - Navigations
@@ -47,42 +61,9 @@ class CardDetailViewController: UIViewController,UITableViewDataSource {
 		self.navigationController!.pushViewController(view, animated: true)
 	}
 	
-	// MARK: - Unbind
-	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		if indexPath.section == 2 && indexPath.row == 0 {
-			// TODO: - 网络请求
-			let alert = UIAlertController(title:"解绑会员卡", message:"您确定要解绑该会员卡吗？", preferredStyle:.alert)
-			let ok = UIAlertAction(title:"确定", style:.default, handler:{ action in
-				ServerConnector.unbindCard(merchantID: self.merchantID!, cardNum: (self.card?.number)!){ result in
-					let alert:UIAlertController!
-					let okAction:UIAlertAction!
-					if result {
-						alert = UIAlertController(title:"提示", message:"会员卡解绑成功！", preferredStyle:.alert)
-						okAction = UIAlertAction(title:"确定", style:.default, handler:{ action in
-							self.navigationController!.popViewController(animated: true)
-						})
-					}
-					else {
-						alert = UIAlertController(title:"提示", message:"会员卡解绑失败！", preferredStyle:.alert)
-						okAction = UIAlertAction(title:"确定", style:.default, handler:nil)
-					}
-					
-					alert.addAction(okAction)
-					self.present(alert, animated: true, completion: nil)
-				}
-			})
-			let cancel = UIAlertAction(title:"取消", style:.cancel, handler:{ action in
-			})
-			alert.addAction(ok)
-			alert.addAction(cancel)
-			self.present(alert, animated: true, completion: nil)
-			// ...
-			
-		}
-	}
 
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 4
+		return 5
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -104,9 +85,56 @@ class CardDetailViewController: UIViewController,UITableViewDataSource {
 			if card != nil {
 				(cell.viewWithTag(1) as! UILabel).text = card?.number
 			}
-		default:
+		case 3:
 			cell = self.tableView.dequeueReusableCell(withIdentifier: "ruleCell")!
+		default:
+			cell = self.tableView.dequeueReusableCell(withIdentifier: "unbindCell")!
 		}
+		cell.selectionStyle = UITableViewCellSelectionStyle.none
 		return cell
+	}
+	@IBAction func clickBarBtn(_ sender: Any) {
+		if isFold{
+			self.cardInfoBackgroundImage.image = UIImage(named: "cardInfo_unfold")
+			isFold = false
+			UIView.animate(withDuration: 0.5){
+				self.backgroundView.frame = CGRect(x: self.backgroundView.frame.origin.x, y: self.backgroundView.frame.origin.y+169, width: self.backgroundView.frame.width, height: self.backgroundView.frame.height)
+			}
+		}else{
+			self.cardInfoBackgroundImage.image = UIImage(named: "cardInfo_fold")
+			isFold = true
+			UIView.animate(withDuration: 0.5){
+				self.backgroundView.frame = CGRect(x: self.backgroundView.frame.origin.x, y: self.backgroundView.frame.origin.y-169, width: self.backgroundView.frame.width, height: self.backgroundView.frame.height)
+			}
+		}
+		
+	}
+	@IBAction func clickUnbind(_ sender: Any) {
+		let alert = UIAlertController(title:"解绑会员卡", message:"您确定要解绑该会员卡吗？", preferredStyle:.alert)
+		let ok = UIAlertAction(title:"确定", style:.default, handler:{ action in
+			ServerConnector.unbindCard(merchantID: self.merchantID!, cardNum: (self.card?.number)!){ result in
+				let alert:UIAlertController!
+				let okAction:UIAlertAction!
+				if result {
+					alert = UIAlertController(title:"提示", message:"会员卡解绑成功！", preferredStyle:.alert)
+					okAction = UIAlertAction(title:"确定", style:.default, handler:{ action in
+						self.navigationController!.popViewController(animated: true)
+					})
+				}
+				else {
+					alert = UIAlertController(title:"提示", message:"会员卡解绑失败！", preferredStyle:.alert)
+					okAction = UIAlertAction(title:"确定", style:.default, handler:nil)
+				}
+				
+				alert.addAction(okAction)
+				self.present(alert, animated: true, completion: nil)
+			}
+		})
+		let cancel = UIAlertAction(title:"取消", style:.cancel, handler:{ action in
+		})
+		alert.addAction(ok)
+		alert.addAction(cancel)
+		self.present(alert, animated: true, completion: nil)
+		// ...
 	}
 }
