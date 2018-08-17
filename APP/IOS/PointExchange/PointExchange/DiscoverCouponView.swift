bf3dede32f160979d8e465351bf73112066e980f
PAN <810650217@qq.com>
2018-08-17 15:26:52 +0800

    修改

--- APP/IOS/PointExchange/PointExchange/DiscoverCouponView.swift
+++ APP/IOS/PointExchange/PointExchange/DiscoverCouponView.swift
@@ -19,7 +19,11 @@ class DiscoverCouponView: UIScrollView {
 	@IBOutlet weak var image2: UIImageView!
 	
 	@IBOutlet weak var image3: UIImageView!
+	
 	var images = [UIImageView]()
+	var imageViews = [GradientView]()
+	
+	
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		initViewFromNib()
@@ -36,6 +40,9 @@ class DiscoverCouponView: UIScrollView {
 		self.view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView as! UIScrollView
 		self.view.frame = bounds
 		self.addSubview(view)
+		self.imageViews.append(imageView1)
+		self.imageViews.append(imageView2)
+		self.imageViews.append(imageView3)
 		self.images.append(self.image1)
 		self.images.append(self.image2)
 		self.images.append(self.image3)
