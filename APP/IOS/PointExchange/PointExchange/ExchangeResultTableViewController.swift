f50c23f4f02034d7b09eb2f80e77fa2c48ed69ba
yinnner <yulianwei1998@163.com>
2018-08-21 14:14:02 +0800

    add ExchangeResultTVC & PageVC

---
+++ APP/IOS/PointExchange/PointExchange/ExchangeResultTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ExchangeResultTableViewController.swift
+//  PointExchange
+//
+//  Created by yiner on 2018/8/21.
+//  Copyright © 2018年 WannaWin. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeResultTableViewController: UITableViewController {
+
+    var index: Int?
+
+}
