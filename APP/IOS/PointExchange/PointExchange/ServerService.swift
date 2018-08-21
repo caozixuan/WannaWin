c13f7eac70ff7af50b7e3ec636ff57770fac475a
PAN <810650217@qq.com>
2018-08-21 20:33:04 +0800

    修改

--- APP/IOS/PointExchange/PointExchange/ServerService.swift
+++ APP/IOS/PointExchange/PointExchange/ServerService.swift
@@ -24,7 +24,7 @@ enum ServerService {
     /// 修改密码
     case changePassword(old:String,new:String)
     /// 重置密码验证码
-    case getResetVCode(phoneNum:String, vcode:String)
+    case checkResetVCode(phoneNum:String, vcode:String)
     /// 重置密码
     case resetPassword(phoneNum:String, newPassword:String)
     
@@ -117,7 +117,7 @@ extension ServerService:TargetType {
             return "/account/login"
         case .changePassword:
             return "/account/changePassword"
-        case .getResetVCode:
+        case .checkResetVCode:
             return "/account/vfcode"
         case .resetPassword:
             return "/account/resetPassword"
@@ -227,7 +227,7 @@ extension ServerService:TargetType {
             params["newPassword"] = new
             return .requestParameters(parameters: params, encoding: URLEncoding.default)
             
-        case .getResetVCode(let phoneNum, let vcode):
+        case .checkResetVCode(let phoneNum, let vcode):
             var params:[String:String] = [:]
             params["phoneNum"] = phoneNum
             params["vcode"] = vcode
