b029dd86e0e9d9a8b6bf0c4493181fa11f16247f
Pan <810650217@qq.com>
2018-09-06 16:54:28 +0800

    修改滑动

--- APP/IOS/PointExchange/PointExchange/CardInfoTableViewController.swift
+++ APP/IOS/PointExchange/PointExchange/CardInfoTableViewController.swift
@@ -25,14 +25,14 @@ class CardInfoTableViewController: UITableViewController {
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
         
-        searchController = UISearchController(searchResultsController: nil)
-        searchController?.searchBar.delegate = self
-        searchController?.searchResultsUpdater = self
-        searchController?.searchBar.searchBarStyle = .minimal
-        searchController?.dimsBackgroundDuringPresentation = true
-        self.definesPresentationContext = true
-        searchController?.hidesNavigationBarDuringPresentation = false
-        self.tableView.tableHeaderView = searchController?.searchBar
+//        searchController = UISearchController(searchResultsController: nil)
+//        searchController?.searchBar.delegate = self
+//        searchController?.searchResultsUpdater = self
+//        searchController?.searchBar.searchBarStyle = .minimal
+//        searchController?.dimsBackgroundDuringPresentation = true
+//        self.definesPresentationContext = true
+//        searchController?.hidesNavigationBarDuringPresentation = false
+//        self.tableView.tableHeaderView = searchController?.searchBar
         
 		// 加入“添加”按钮在导航栏右边
 		let addBtn = UIBarButtonItem(barButtonSystemItem:UIBarButtonSystemItem.add , target: self, action: #selector(goAddVC))
@@ -75,39 +75,39 @@ class CardInfoTableViewController: UITableViewController {
 	//MARK: - Table view data source
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if searchResult == nil || searchResult?.count == 0 {
+//        if searchResult == nil || searchResult?.count == 0 {
             if let array = cardArray {
                 return array.count
             }
             else{
                 return 0
             }
-        }else{
-            return (searchResult?.count)!
-        }
+//        }else{
+//            return (searchResult?.count)!
+//        }
         
 	}
 	
 	 override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
 		let cell  = tableView.dequeueReusableCell(withIdentifier: "card", for: indexPath)
-        if searchResult != nil && searchResult?.count != 0{
+//        if searchResult != nil && searchResult?.count != 0{
             (cell.viewWithTag(1) as! UIImageView).imageFromURL((searchResult![indexPath.row].merchant?.logoURL)!, placeholder: UIImage())
             (cell.viewWithTag(2) as! UILabel).text = String(stringInterpolationSegment: searchResult![indexPath.row].points)
             (cell.viewWithTag(3) as! UILabel).text = String(stringInterpolationSegment: searchResult![indexPath.row].points*searchResult![indexPath.row].proportion!)
             (cell.viewWithTag(4) as! UILabel).text = searchResult![indexPath.row].merchant?.name
             (cell.viewWithTag(5) as! UIImageView).image = UIImage(named: "bg2_\(searchResult![indexPath.row].cardStyle!)")
-            
-        }else{
-            if cardArray != nil {
-                (cell.viewWithTag(1) as! UIImageView).imageFromURL((cardArray![indexPath.row].merchant?.logoURL)!, placeholder: UIImage())
-                (cell.viewWithTag(2) as! UILabel).text = String(stringInterpolationSegment: cardArray![indexPath.row].points)
-                (cell.viewWithTag(3) as! UILabel).text = String(stringInterpolationSegment: cardArray![indexPath.row].points*cardArray![indexPath.row].proportion!)
-                (cell.viewWithTag(4) as! UILabel).text = cardArray![indexPath.row].merchant?.name
-                (cell.viewWithTag(5) as! UIImageView).image = UIImage(named: "bg2_\(cardArray![indexPath.row].cardStyle!)")
-                
-            }
-        }
+//
+//        }else{
+//            if cardArray != nil {
+//                (cell.viewWithTag(1) as! UIImageView).imageFromURL((cardArray![indexPath.row].merchant?.logoURL)!, placeholder: UIImage())
+//                (cell.viewWithTag(2) as! UILabel).text = String(stringInterpolationSegment: cardArray![indexPath.row].points)
+//                (cell.viewWithTag(3) as! UILabel).text = String(stringInterpolationSegment: cardArray![indexPath.row].points*cardArray![indexPath.row].proportion!)
+//                (cell.viewWithTag(4) as! UILabel).text = cardArray![indexPath.row].merchant?.name
+//                (cell.viewWithTag(5) as! UIImageView).image = UIImage(named: "bg2_\(cardArray![indexPath.row].cardStyle!)")
+//
+//            }
+//        }
         cell.selectionStyle = .none
         
 		return cell
@@ -117,40 +117,40 @@ class CardInfoTableViewController: UITableViewController {
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let sb = UIStoryboard(name: "HomePage", bundle: nil)
         let vc = sb.instantiateViewController(withIdentifier: "CardDetailTableViewController") as! CardDetailViewController
-        if searchResult != nil && searchResult?.count != 0{
-            vc.merchantID = searchResult?[indexPath.row].merchant?.id
-        }else{
+//        if searchResult != nil && searchResult?.count != 0{
+//            vc.merchantID = searchResult?[indexPath.row].merchant?.id
+//        }else{
             vc.merchantID = cardArray?[indexPath.row].merchant?.id
-        }
+//        }
         self.navigationController?.pushViewController(vc, animated: true)
 		
 	}
 }
  
- extension CardInfoTableViewController:UISearchBarDelegate,UISearchResultsUpdating{
-    //点击搜索按钮，触发该代理方法，如果已经显示搜索结果，那么直接去除键盘，否则刷新列表
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        if searchResult != nil && searchResult?.count != 0{
-            tableView.reloadData()
-            searchController?.searchBar.resignFirstResponder()
-        }
-    }
-    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
-        searchController?.searchBar.resignFirstResponder()
-    }
-    //这个updateSearchResultsForSearchController(_:)方法是UISearchResultsUpdating中唯一一个我们必须实现的方法。当search bar 成为第一响应者，或者search bar中的内容被改变将触发该方法.不管用户输入还是删除search bar的text，UISearchController都会被通知到并执行上述方法。
-    func updateSearchResults(for searchController: UISearchController) {
-        let searchString = searchController.searchBar.text
-        isSearch = true
-        //过滤数据源，存储匹配的数据
-        searchResult = cardArray?.filter({ (card) -> Bool in
-            let name: NSString = card.merchant?.name as! NSString
-            return   (name.range(of: searchString!, options: .caseInsensitive).location) != NSNotFound
-        })
-        //刷新表格
-        tableView.reloadData()
-    }
-
-    
-    
- }
+// extension CardInfoTableViewController:UISearchBarDelegate,UISearchResultsUpdating{
+//    //点击搜索按钮，触发该代理方法，如果已经显示搜索结果，那么直接去除键盘，否则刷新列表
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        if searchResult != nil && searchResult?.count != 0{
+//            tableView.reloadData()
+//            searchController?.searchBar.resignFirstResponder()
+//        }
+//    }
+//    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+//        searchController?.searchBar.resignFirstResponder()
+//    }
+//    //这个updateSearchResultsForSearchController(_:)方法是UISearchResultsUpdating中唯一一个我们必须实现的方法。当search bar 成为第一响应者，或者search bar中的内容被改变将触发该方法.不管用户输入还是删除search bar的text，UISearchController都会被通知到并执行上述方法。
+//    func updateSearchResults(for searchController: UISearchController) {
+//        let searchString = searchController.searchBar.text
+//        isSearch = true
+//        //过滤数据源，存储匹配的数据
+//        searchResult = cardArray?.filter({ (card) -> Bool in
+//            let name: NSString = card.merchant?.name as! NSString
+//            return   (name.range(of: searchString!, options: .caseInsensitive).location) != NSNotFound
+//        })
+//        //刷新表格
+//        tableView.reloadData()
+//    }
+//
+//
+//
+// }
