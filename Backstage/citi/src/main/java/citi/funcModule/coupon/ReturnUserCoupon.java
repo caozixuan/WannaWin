b6cb350ef180bd77f401ab69e1f0346b2ae96f25
曹子轩 <caozixuanchina@gmail.com>
2018-09-07 22:50:54 +0800

    修改string为空bug

--- Backstage/citi/src/main/java/citi/funcModule/coupon/ReturnUserCoupon.java
+++ Backstage/citi/src/main/java/citi/funcModule/coupon/ReturnUserCoupon.java
@@ -3,6 +3,7 @@ package citi.funcModule.coupon;
 import citi.vo.UserCoupon;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 public class ReturnUserCoupon extends UserCoupon {
     private String itemName;
@@ -12,7 +13,12 @@ public class ReturnUserCoupon extends UserCoupon {
     private String logoURL;
 
     public ReturnUserCoupon(long couponID, String userID, String itemID, String state, String getTime, String useTime, String itemName, int points, String description, String overdueTime, String logoURL) {
-        super(couponID, userID, itemID, state, Timestamp.valueOf(getTime), Timestamp.valueOf(useTime));
+        this.couponID = couponID;
+        this.userID = userID;
+        ItemID = itemID;
+        this.state = state;
+        this.getTime = getTime;
+        this.useTime = useTime;
         this.itemName = itemName;
         this.points=points;
         this.description = description;
