6e4d20d1ae7bb8e00f5de297182f8ec35f06d6ed
曹子轩 <caozixuanchina@gmail.com>
2018-09-10 22:15:58 +0800

    活动搜索修改

--- Backstage/citi/src/main/java/citi/funcModule/item/ItemController.java
+++ Backstage/citi/src/main/java/citi/funcModule/item/ItemController.java
@@ -80,7 +80,7 @@ public class ItemController {
     @ResponseBody
     @RequestMapping("/search")
     public String search(String keyword, String start, String end){
-        if(Integer.valueOf(start)>=Integer.valueOf(end)||keyword==null)
+        if(Integer.valueOf(start)>=Integer.valueOf(end)||keyword==null||keyword.equals(""))
             return "[]";
         String [] keywords = keyword.split("\\s+");
         ArrayList<ItemBean> itemBeans = itemService.search(keywords);
